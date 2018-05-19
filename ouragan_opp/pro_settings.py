33014c70459411aff3a80b2fda66589b23da09dd
germain kataku <germainkataku@hotmail.com>
2018-05-19 10:29:17 +0100

    Add production settings

---
+++ ouragan_opp/pro_settings.py
@@ -0,0 +1,8 @@
+from ouragan_opp.settings import *
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+SECRET_KEY = 's0=lzz&l0xdp!8+)hf6_1me7f0mx(h42yr_tq59i=8n_4i25&h'
+
+ALLOWED_HOSTS = ['ouragan.herokuapp.com']
