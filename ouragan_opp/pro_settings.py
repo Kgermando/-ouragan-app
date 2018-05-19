c6637d34dc8531d0f51909e34124eb835ca56bb9
germain kataku <germainkataku@hotmail.com>
2018-05-19 10:49:01 +0100

    Configure PostgreSQL

--- ouragan_opp/pro_settings.py
+++ ouragan_opp/pro_settings.py
@@ -1,7 +1,10 @@
+import dj_database_url
 from ouragan_opp.settings import *
 
 DEBUG = False
 TEMPLATE_DEBUG = False
+
+DATABASES['default'] = dj_database_url.config()
 
 SECRET_KEY = 's0=lzz&l0xdp!8+)hf6_1me7f0mx(h42yr_tq59i=8n_4i25&h'
 
